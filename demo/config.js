58e11f9491692e4216b773d22b07e738b096d1a6
phinney <phinney001@qq.com>
2018-01-15 15:08:11 +0800

    add config

---
+++ demo/config.js
@@ -0,0 +1,8 @@
+var config = {
+    SecretId: 'AKIDMbhju36ReigVArnE9sPlwhKl2A9wtyCK',
+    SecretKey: '85uXyDH9pOuFOK4d1DD2jdNvLZcXHqCQ',
+    Bucket: 'test-1255428994',
+    Region: 'ap-chengdu'
+};
+
+module.exports = config;
